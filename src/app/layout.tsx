445e47914f3e245bcda5a4c9db0ed9c2d983aae5
rian <rianaugusto2018@gmail.com>
2025-10-27 13:36:07 +0000

    CORRIGINDO, QUERO UM FAVICON PARA O MEU SITE, COM ESSA IMAGEM: https://i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <head>
+        <link rel="icon" href="https://i.imgur.com/2U2l5aD.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
