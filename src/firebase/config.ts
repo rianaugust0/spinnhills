057e70b768ad16437a6aab03efb8d0dfcb887d76
rian <rianaugusto2018@gmail.com>
2026-01-09 20:18:14 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,4 +2,11 @@
 // It will be populated with your Firebase project configuration.
 // For now, we'll use a placeholder to allow the app to compile.
 
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "hillscut-23763634-d28b8",
+  "appId": "1:178308164536:web:b86e7a993b7e3991247d4d",
+  "apiKey": "AIzaSyDow-JJwe7PYFUkEiXKvwj2nqomov1pfH4",
+  "authDomain": "hillscut-23763634-d28b8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "178308164536"
+};
