8a0df17b69ec52b537bc40c32f9e6c4ebabf688b
rian <rianaugusto2018@gmail.com>
2026-01-11 17:51:34 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
+        protocol: 'https',
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
